252ad3026e6f51cc174233215ea272c3ec5c5dbc
Aris Lancrescent <Aris@Ariss-MacBook-Pro.local>
2015-04-08 20:04:54 +0200

    Initial commit

---
+++ Identity.cs
@@ -0,0 +1,17 @@
+﻿using ICities;
+
+namespace RemoveSeagulls
+{
+    public class Identity : IUserMod
+    {
+        public string Name
+        {
+            get { return Settings.Instance.Tag; }
+        }
+
+        public string Description
+        {
+            get { return "Nonpermanently removes all seagulls. Disable to get the seagulls back. Requires [ARIS] Skylines Overwatch."; }
+        }
+    }
+}
---
+++ Settings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RemoveSeagulls
+{
+    public sealed class Settings
+    {
+        private Settings()
+        {
+            Tag = "[ARIS] Remove Seagulls";
+        }
+
+        private static readonly Settings _Instance = new Settings();
+        public static Settings Instance { get { return _Instance; } }
+
+        public readonly string Tag;
+    }
+}
